62ffc8dfa58b0a4bf9ee6e7f3b8514cf6bc47de7
trunk <trunk@trunk-Lesovo.(none)>
2013-05-28 22:01:05 +0800

    add listen

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
-var http = require('http'); 
-function start(){ 
-    server = http.createServer(function (req, res) {   
-          res.writeHeader(200, {"Content-Type": "text/plain"});   
-          res.end("Hello edwin\n");   
-    })   
-    server.listen(80);   
-    console.log("httpd start @80");   
-} 
+var http = require("http");
+var url = require("url");
+
+function logHeader(req) {
+    console.log(req.url);
+}
+
+function postListen(postDataChunk) {
+    console.log(postDataChunk);
+}
+
+function endListen() {
+    console.log("\n--------\n");
+}
+
+function start() {
+    server = http.createServer(function (req, res) {
+        var postData = "";
+        var pathname = url.parse(req.url).pathname;
+
+        logHeader(req);
+        req.addListener("data", postListen);
+        req.addListener("end", endListen);
+
+        res.writeHeader(200, {"Content-Type": "text/plain"}); 
+        res.write("\n--------\n");
+        res.write(req.url);
+        res.write("\n--------\n");
+        res.end();
+    })
+    server.listen(80);
+    console.log("httpd start @80");
+}
+
 exports.start = start;
